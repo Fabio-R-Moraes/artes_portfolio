140843a4e7a483350cd61b774fa5a88563408f00
Fábio Rodrigues de Moraes <fmoraes05@outlook.com>
2025-06-04 19:55:00 -0300

    Criando as tabelas do banco de dados

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Category(models.Model):
+    nome =  models.CharField(max_length=65)
+
+class Photos(models.Model):
+    titulo = models.CharField(max_length=65)
+    descricao = models.CharField(max_length=165)
+    slug = models.SlugField()
+    historia = models.TextField()
+    historia_html = models.BooleanField(default=False)
+    preco = models.DecimalField(decimal_places=2, max_digits=9)
+    criado_em = models.DateTimeField(auto_now_add=True)
+    atualizado_em = models.DateTimeField(auto_now=True)
+    esta_publicado = models.BooleanField(default=False)
+    photo_image = models.ImageField(upload_to='portfolio/portfolio_image/%d/%m/%Y')
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL, null=True,
+    )
+    author = models.ForeignKey(
+        User, on_delete=models.SET_NULL, null=True,
+    )
