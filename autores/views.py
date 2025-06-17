1136aef61707e0005349e2fbcfb6cf5a34c8b331
Fábio Rodrigues de Moraes <fmoraes05@outlook.com>
2025-06-17 20:02:40 -0300

    Cuidando do formulário de registro para usuários

--- autores/views.py
+++ autores/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from .forms import RegisterForm
 from django.http import Http404
+from django.contrib import messages
 
 def register_view(request):
     request.session['numero'] = request.session.get('numero') or 1
@@ -19,5 +20,12 @@ def register_create(request):
     POST = request.POST
     request.session['register_form_data'] = POST
     form = RegisterForm(POST)
+
+    if form.is_valid():
+        usuario = form.save(commit=False)
+        usuario.set_password(usuario.password)
+        usuario.save()
+        messages.success(request, 'Seu usuário foi criado, por favor, faça o login...')
+        del(request.session['register_form_data'])
 
     return redirect('autores:register')
